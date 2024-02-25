aab08e72c8383c0e024839b6be4be97c1f6ef952
maltiez2 <maltiez2@gmail.com>
2024-02-25 08:56:20 +0300

    Added console bips test

--- instrumentsframework/instrumentsframework/source/InstrumentsFrameworkModSystem.cs
+++ instrumentsframework/instrumentsframework/source/InstrumentsFrameworkModSystem.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TextPlayer;
+using TextPlayer.ABC;
 using Vintagestory.API.Common;
 
 namespace InstrumentsFramework;
@@ -10,9 +13,42 @@ namespace InstrumentsFramework;
 public class InstrumentsFrameworkModSystem : ModSystem
 {
     private ICoreAPI? mApi;
-    
+    private Player? mPlayer;
+
     public override void Start(ICoreAPI api)
     {
         mApi = api;
+
+        string filePath = "C:\\Users\\user\\Desktop\\abc\\ACDC - Highway to Hell.abc";
+
+        mPlayer = new();
+
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            mPlayer.Load(reader);
+        }
+
+        mPlayer.Play();
+
+        if (api.Side == EnumAppSide.Server) api.World.RegisterGameTickListener(_ => mPlayer?.Update(), 0);
+    }
+}
+
+public class Player : ABCPlayer
+{
+    public Player() : base(strict: false)
+    {
+        Settings.MaxSize = (int)1e5;
+    }
+    
+    protected override void PlayNote(Note note, int channel, TimeSpan time)
+    {
+        Console.WriteLine($"{channel} - {note} - {time}");
+
+        if (channel <= 1)
+        {
+            // length is halved because Console.Beep messes up using full length on consecutive notes
+            Console.Beep((int)note.GetFrequency(), (int)(note.Length.TotalMilliseconds * 0.5));
+        }
     }
 }
