d4f0837dd1f528df1cc4dbad9a885c59786f94f0
maltiez2 <maltiez2@gmail.com>
2024-02-25 07:27:44 +0300

    Add project files.

---
+++ instrumentsframework/instrumentsframework/source/InstrumentsFrameworkModSystem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+
+namespace InstrumentsFramework;
+
+public class InstrumentsFrameworkModSystem : ModSystem
+{
+    private ICoreAPI? mApi;
+    
+    public override void Start(ICoreAPI api)
+    {
+        mApi = api;
+    }
+}
